d7286b304d87e3aed8cbc29084ba9e251b0b52ca
michael <michael.lcw99@gmail.com>
2021-06-23 17:47:01 +0800

    added localstorage and remove all books feature

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 let myLibrary = [];
 let nextId = 0;
+
 const background = document.querySelector("main");
+const form = document.querySelector(".book-form");
+const bookshelf = document.querySelector("section");
+
+const addBookBtn = document.querySelector("#add-book");
+const removeBooksBtn = document.querySelector("#remove-books");
+const closeFormBtn = document.querySelector("#close-form");
+const submitBtn = document.querySelector("#submit");
+const reminderText = document.querySelector("#reminder-text");
 
 function Book(title, author, numPages) {
     this.title = title;
@@ -10,27 +19,42 @@ function Book(title, author, numPages) {
     this.id = nextId++;
 }
 
+function updateLibrary() {
+    localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+}
+
 function addBookToLibrary(book) {
     myLibrary.push(book);
-    myLibrary.forEach(book => console.log(book));
+    updateLibrary();
 }
 
 function displayForm() {
-    const form = document.querySelector(".book-form");
     form.style.display = 'block';
     background.classList.add("blur");
 }
 
 function closeForm() {
-    const form = document.querySelector(".book-form");
     form.style.display = 'none';
     background.classList.remove("blur");
+    reminderText.textContent = '';
+}
+
+function validateForm(title, author, numPages) {
+    if (!(title && author && numPages)) {
+        reminderText.textContent = 'Please fill up accordingly!';
+    } else {
+        reminderText.textContent = '';
+        const newBook = new Book(title, author, numPages);
+        addBookToLibrary(newBook);
+        closeForm();
+        displayBook(newBook);
+        background.classList.remove("blur");
+    }
 }
 
 function displayBook(book) {
-    const bookshelf = document.querySelector("section");
     const bookContainer = document.createElement("div");
-    const bookDisplay = document.createElement("div");
+    const bookDisplay = document.createElement("div"); 
     bookContainer.setAttribute("data-id", book.id); 
 
     // styling
@@ -53,7 +77,7 @@ function displayBook(book) {
         bookshelf.removeChild(bookContainer);
         const index = myLibrary.findIndex(b => b.id === book.id);
         myLibrary.splice(index, 1);
-        myLibrary.forEach(book => console.log(book));
+        updateLibrary();
     });
     
     // create readBtn
@@ -75,6 +99,7 @@ function displayBook(book) {
             readStatusText.textContent = "";
             book.completed = false;
         }
+        updateLibrary();
     });
 
     // adding
@@ -83,23 +108,36 @@ function displayBook(book) {
     bookContainer.appendChild(btnContainer);
     bookContainer.appendChild(bookDisplay);
     bookshelf.appendChild(bookContainer);
+
+    console.log(myLibrary); //
 }
 
-const addBookBtn = document.querySelector("#add-book");
 addBookBtn.addEventListener("click", displayForm);
-
-const closeFormBtn = document.querySelector("#close-form");
+removeBooksBtn.addEventListener("click", function() {
+    const delButtons = document.querySelectorAll("#delete");
+    delButtons.forEach(btn => btn.click());
+    console.log(myLibrary);
+});
 closeFormBtn.addEventListener("click", closeForm);
-
-const submitBtn = document.querySelector("#submit");
 submitBtn.addEventListener("click", function() {
     const title = document.querySelector("input#title").value;
     const author = document.querySelector("input#author").value;
     const numPages = document.querySelector("input#numPages").value;
 
-    const newBook = new Book(title, author, numPages);
-    addBookToLibrary(newBook);
-    closeForm();
-    displayBook(newBook);
-    background.classList.remove("blur");
+    validateForm(title, author, numPages);
 });
+
+// local storage
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+});
+
+
+document.body.onload = function() {
+    myLibrary = JSON.parse(localStorage.getItem("myLibrary"));
+    if (myLibrary === null) {
+        myLibrary = [];
+    } else {
+        myLibrary.forEach(book => displayBook(book));
+    }
+}
